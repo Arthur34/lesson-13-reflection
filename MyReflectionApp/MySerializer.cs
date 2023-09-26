6b338f9c38ec63361f2b4df036411f87def8e1e1
Arthur Matyukov <m.arthur@outlook.com>
2023-09-26 20:58:39 +0300

    Занятие 13: Отражение (Reflection) // ДЗ

---
+++ MyReflectionApp/MySerializer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReflectionApp
+{
+    public class MySerializer
+    {
+        /// <summary>
+        /// Разделитель полей
+        /// </summary>
+        private static readonly char _fieldSep = ';';
+
+        /// <summary>
+        /// Разделитель строк
+        /// </summary>
+        private static readonly string _lineSep = Environment.NewLine;
+
+        /// <summary>
+        /// Сериализация в csv
+        /// </summary>
+        /// <param name="o">Объект для сериализации</param>
+        /// <returns>CSV</returns>
+        public static string ObjToCsv(object o)
+        {
+            var objType = o.GetType();
+            var fieldList = objType.GetFields();
+            var serializeList = new Dictionary<string, string>();
+
+            for (int i = 0; i < fieldList.Length; i++)
+                serializeList.Add(fieldList[i].Name, value: fieldList[i].GetValue(o).ToString());
+
+            var csvString = new StringBuilder();
+            csvString.AppendJoin(_fieldSep, serializeList.Keys).Append(Environment.NewLine);
+            csvString.AppendJoin(_fieldSep, serializeList.Values);
+
+            return csvString.ToString();
+        }
+
+        /// <summary>
+        /// Десериализация объекта из csv
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="csv"></param>
+        /// <returns></returns>
+        public static object CsvToObj<T>(string csv) where T : class, new()
+        {
+            var objType = typeof(T);
+            object obj = new T();
+
+            var lines = csv.Split(_lineSep);
+            var fields = lines[0].Split(_fieldSep);
+            var values = lines[1].Split(_fieldSep);
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var field = objType.GetField(fields[i]);
+                if (field == null)
+                    continue;
+                field.SetValue(obj, Convert.ChangeType(values[i], field.FieldType));
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ MyReflectionApp/Program.cs
@@ -0,0 +1,127 @@
+﻿using MyReflectionApp;
+using MyReflectionClassLibrary;
+using System.Diagnostics;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var myF = new F().Get();
+
+        // количество итераций
+        int iterations = 100000;
+
+        // csv
+        var csvTime1 = TestSerializeToCsv(myF, iterations, false);
+        var csvTime2 = TestSerializeToCsv(myF, iterations, true);
+        string csv = MySerializer.ObjToCsv(myF);
+        var csvTime3 = TestDeserializeFromCsv(csv, iterations);
+
+        // json
+        var jsonTime1 = TestSerializeToJson(myF, iterations, false);
+        var jsonTime2 = TestSerializeToJson(myF, iterations, true);
+        string json = JsonSerializer.Serialize(myF, new JsonSerializerOptions { IncludeFields = true });
+        var jsonTime3 = TestDeserializeFromJson<F>(json, iterations);
+
+        // выводим результаты
+        Console.WriteLine($"\nIterations count = {iterations}");
+
+        Console.WriteLine($"\nMy reflection (csv):");
+        Console.WriteLine($"Serialization without console output: {csvTime1} ms");
+        Console.WriteLine($"Serialization with console output: {csvTime2} ms");
+        Console.WriteLine($"Deserialization: {csvTime3} ms");
+
+        Console.WriteLine($"\nStandard System.Text.Json.JsonSerializer:");
+        Console.WriteLine($"Serialization without console output: {jsonTime1} ms");
+        Console.WriteLine($"Serialization with console output: {jsonTime2} ms");
+        Console.WriteLine($"Deserialization: {jsonTime3} ms");
+
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Тестирование сериализации в CSV.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t">Объект для сериализации</param>
+    /// <param name="iterations">Количество итераций</param>
+    /// <param name="isShow">Выводить в консоль</param>
+    /// <returns></returns>
+    static long TestSerializeToCsv<T>(T t, int iterations, bool isShow = false) where T : class
+    {
+        var timer = Stopwatch.StartNew();
+        string csv;
+
+        for (int i = 0; i <= iterations; i++)
+        {
+            csv = MySerializer.ObjToCsv(t);
+            if (isShow)
+                Console.WriteLine($"#{i}\n{csv}");
+        }
+
+        timer.Stop();
+        return timer.ElapsedMilliseconds;
+    }
+
+    /// <summary>
+    /// Тестирование сериализации в JSON.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t">Объект для сериализации</param>
+    /// <param name="iterations">Количество итераций</param>
+    /// <param name="isShow">Выводить в консоль</param>
+    /// <returns></returns>
+    static long TestSerializeToJson<T>(T t, int iterations, bool isShow = false) where T : class
+    {
+        string json;
+        var timer = Stopwatch.StartNew();
+
+        for (int i = 0; i <= iterations; i++)
+        {
+            json = JsonSerializer.Serialize(t, new JsonSerializerOptions { IncludeFields = true });
+            if (isShow)
+                Console.WriteLine($"#{i}\n{json}");
+        }
+
+        timer.Stop();
+        return timer.ElapsedMilliseconds;
+    }
+
+    /// <summary>
+    /// Тестирование десериализации из CSV.
+    /// </summary>
+    /// <param name="csv">Сериализованная строка CSV</param>
+    /// <param name="iterations">Количество итераций</param>
+    /// <returns></returns>
+    static long TestDeserializeFromCsv(string csv, int iterations)
+    {
+        var timer = Stopwatch.StartNew();
+
+        for (int i = 0; i <= iterations; i++)
+        {
+            var v = MySerializer.CsvToObj<F>(csv);
+        }
+
+        timer.Stop();
+        return timer.ElapsedMilliseconds;
+    }
+
+    /// <summary>
+    /// Тестирование десериализации из JSON.
+    /// </summary>
+    /// <typeparam name="T">Тип</typeparam>
+    /// <param name="json">Сериализованная строка JSON</param>
+    /// <param name="iterations">Кол-во итераций</param>
+    /// <returns></returns>
+    static long TestDeserializeFromJson<T>(string json, int iterations)
+    {
+        var timer = Stopwatch.StartNew();
+        for (int i = 0; i <= iterations; i++)
+        {
+            var v = JsonSerializer.Deserialize(json, typeof(T), new JsonSerializerOptions { IncludeFields = true });
+        }
+        timer.Stop();
+        return timer.ElapsedMilliseconds;
+    }
+}
